4c42a33eb9120bdccad5f8b56706000815947d43
Adam Sepulveda <adam.sepulveda@revature.net>
2021-06-02 21:13:10 -0500

    adds rps assignment

---
+++ HelloWorldProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"My name is {name} and I am {age} years old");
+        }
+    }
+}
